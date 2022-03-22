5276f708913c51fb05af4a2f1a441d7fa92bf561
Arun <rarnkmrbridgelabz.com>
2022-03-22 14:19:18 +0530

    Added UC-6

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -10,34 +10,32 @@ public class TicTacToe {
         System.out.println("Welcome to TicTacToe Workshop");
 
         for (int i = 1; i < board.length; i++) {
-            board[i] =' ';
+            board[i] = ' ';
             System.out.println(board[i]);
         }
-
-        char player1 = choise();
-        char computer;
-        if (player1 == 'X'||player1=='x') {
-            player1 = 'X';
-            computer = 'O';
-        } else {
-            player1 = 'O';
-            computer = 'X';
-        }
-        System.out.println("Player1 Chosen Letter is: " + player1);
-        System.out.println("Computer Chosen Letter is: " + computer);
-        showBoard(board);
+        choise();
+        showBoard();
+        toss();
         int move = userMove();
         checkFreeSpace(move);
     }
+    public  static void choise() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Choose a Letter X or O:");
+            player1 = sc.next().charAt(0);
+            if (player1 == 'X' || player1 == 'x') {
+                player1 = 'X';
+                computer = 'O';
+            } else {
+                player1 = 'O';
+                computer = 'X';
+            }
+            System.out.println("Player1 Chosen Letter is: " + player1);
+            System.out.println("Computer Chosen Letter is: " + computer);
+        }
 
-    public static char choise() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Choose a Letter X or O:");
-        char p1 = sc.next().charAt(0);
-        return p1;
-    }
 
-    public static void showBoard(char[] board) {
+    public static void showBoard() {
         for (int i = 1; i < board.length; i=i+3) {
             System.out.println("|_"+board[i]+"_|_" + board[i + 1]+"_|_" + board[i + 2]+"_|");
         }
@@ -57,6 +55,15 @@ public class TicTacToe {
             System.out.println("Space already Occupied,Select anyother board index position from 1 to 9:");
             move=userMove();
             checkFreeSpace(move);
+        }
+    }
+    public static void toss(){
+        int coin = (int) (Math.random()*2);
+        if(coin == 1){
+            System.out.println("Player 1 won the Toss and start:");
+        }
+        else{
+            System.out.println("Computer won the Toss and start:");
         }
     }
 
