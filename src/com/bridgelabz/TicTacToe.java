9101a034c2d95925c5a04da75cd0e5e1c45a5bd6
Arun <rarnkmrbridgelabz.com>
2022-03-22 12:03:19 +0530

    Added UC-3

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -7,29 +7,35 @@ public class TicTacToe {
         System.out.println("Welcome to TicTacToe Workshop");
         char[] board = new char[10];
         for (int i = 1; i < board.length; i++) {
-            board[i] = ' ';
+            board[i] =' ';
             System.out.println(board[i]);
         }
         char player1 = choise();
         char computer;
-        if(player1 == 'X'){
+        if (player1 == 'X'||player1=='x') {
             player1 = 'X';
             computer = 'O';
-        }
-        else{
+        } else {
             player1 = 'O';
             computer = 'X';
         }
-        System.out.println("Player1 Chosen Letter is: "+player1);
-        System.out.println("Computer Chosen Letter is: "+computer);
+        System.out.println("Player1 Chosen Letter is: " + player1);
+        System.out.println("Computer Chosen Letter is: " + computer);
+        showBoard(board);
     }
 
-    public static char choise(){
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Choose a Letter X or O:");
-                char p1 = sc.next().charAt(0);
-                return p1;
-            }
+    public static char choise() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose a Letter X or O:");
+        char p1 = sc.next().charAt(0);
+        return p1;
+    }
+    public static void showBoard(char[] board) {
+        for (int i = 1; i < board.length; i=i+3) {
+            System.out.println("|_"+board[i]+"_|_" + board[i + 1]+"_|_" + board[i + 2]+"_|");
         }
+    }
+}
+
 
 
